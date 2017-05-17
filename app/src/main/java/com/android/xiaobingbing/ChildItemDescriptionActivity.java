25bdc5fdc7f3f226861362c67f7715807a79cb4b
liurenyi <928171620@qq.com>
2017-05-17 16:06:56 +0800

    创建webview

--- app/src/main/java/com/android/xiaobingbing/ChildItemDescriptionActivity.java
+++ app/src/main/java/com/android/xiaobingbing/ChildItemDescriptionActivity.java
@@ -1,9 +1,13 @@
 package com.android.xiaobingbing;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.android.xiaobingbing.util.GameManager;
@@ -13,6 +17,7 @@ public class ChildItemDescriptionActivity extends AppCompatActivity {
     public static final String TAG = "liu-ChildItemDescriptionActivity";
 
     private TextView childTitle;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +31,34 @@ public class ChildItemDescriptionActivity extends AppCompatActivity {
         String childExtra = intent.getStringExtra("child");
         String groupExtra = intent.getStringExtra("group");
         childTitle = (TextView) this.findViewById(R.id.child_title);
+        webView = (WebView) this.findViewById(R.id.webView);
         if (groupExtra != null) {
             childTitle.setText(groupExtra);
         }
         String url = GameManager.queryDreamSQL(groupExtra, childExtra);
         Log.e(TAG, "url = " + url);
         if (url != null) {
-            // TODO: 2017/5/17
+            webView.loadUrl(url);
+            webView.getSettings().setJavaScriptEnabled(true); // 如果访问的页面中有Javascript，则webview必须设置支持Javascript
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
         }
     }
 
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (webView.canGoBack()) {
+                webView.goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
